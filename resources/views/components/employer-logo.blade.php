adb7396a7a33463cff7a97537d4899a2bdbcf12b
Thomas Jones <Thomasajones@hotmail.co.uk>
2024-10-21 19:11:57 +0100

    Blade Templates

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['width' => 90])
+
+<img src="http://picsum.photos/seed/{{rand(0,72937858578537587)}}/{{$width}}" alt="Job company image" class="rounded-xl">
---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,5 @@
+<div class="inline-flex items-center">
+    <span class="w-2 h-2 bg-white m-1"> </span>
+
+    <h3 class="font-bold text-lg">{{$slot}} </h3>
+</div>
